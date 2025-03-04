7f8ccbc0fbcf846580528401dbb14fa40357e5a1
Pratik Ramesh Rathod <111563411+PratikProgramming@users.noreply.github.com>
2025-03-04 17:05:04 +0530

    Add files via upload

--- Validation.java
+++ Validation.java
@@ -2,11 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package eyetrackingsystem;
-
-import eyetrackingsystem.User.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+package eyetrackingsystem.User;
 
 /**
  *
@@ -14,40 +10,22 @@ import java.util.regex.Pattern;
  */
 public class Validation {
      
-
-     public static String valid(String name,String contact)
+    public static String ContactNo(String contact)
         {
           //  Regex re = new Regex("[0-9]{10}");
 
-            
-        if("".equals(name))
-             return "Invalid Name ...";
-       
-        CharSequence inputStr = name;
-        Pattern pattern = Pattern.compile("^[a-zA-Z \\s]*$");
-        Matcher matcher = pattern.matcher(inputStr);
-        if(matcher.matches())
-        {
-        }
-        else
-        {
-             return "Invalid Name ...";
-         }
-            
-            boolean  c=contact.matches("[0-9]{10}");
-            if(c==false)
+             boolean b;
+             b=contact.matches("[0-9]{10}");
+            if (b)
             {
-                return "Invalid Contact ...";
+                return "";
             }
-            
-          //  [a-zA-Z ]"
-            
-           
-         
-            
-            return "";
+            else
+            {
+                return "Invalid Contact No";
+            }
         }
 
-    
+
     
 }
